edd22d6d335074ce3a830d61e91edec568190174
pgoulida <pgoulidakes@gmail.com>
2026-03-18 13:12:46 +0200

    go

--- go.mod
+++ go.mod
@@ -4,7 +4,6 @@ go 1.25.0
 
 require (
 	github.com/gofrs/uuid v4.4.0+incompatible
-	github.com/joho/godotenv v1.5.1
 	github.com/mattn/go-sqlite3 v1.14.34
 	golang.org/x/crypto v0.48.0
 	golang.org/x/oauth2 v0.36.0
---
+++ internal/config/env.go
@@ -0,0 +1,43 @@
+package config
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func LoadEnv(filename string) error {
+	file, err := os.Open(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	lineNum := 0
+	for scanner.Scan() {
+		lineNum++
+		line := strings.TrimSpace(scanner.Text())
+		if line == "" || strings.HasPrefix(line, "#") {
+			continue
+		}
+
+		parts := strings.SplitN(line, "=", 2)
+		if len(parts) != 2 {
+			return fmt.Errorf("%s:%d invalid env line", filename, lineNum)
+		}
+
+		key := strings.TrimSpace(parts[0])
+		value := strings.TrimSpace(parts[1])
+		if key == "" {
+			return fmt.Errorf("%s:%d empty env key", filename, lineNum)
+		}
+
+		if err := os.Setenv(key, value); err != nil {
+			return err
+		}
+	}
+
+	return scanner.Err()
+}
